f31fc6eaa77e9cfd8f4c39cfe0799e9165f5f78a
Aang23 <qwerty15@gmx.fr>
2019-10-03 16:38:33 +0200

    Reflection for fillStateContainer. Fixes https://github.com/Aang23/UndergroundBiomesConstructs/issues/135

--- src/main/java/com/aang23/undergroundbiomes/blocks/UBOre.java
+++ src/main/java/com/aang23/undergroundbiomes/blocks/UBOre.java
@@ -1,5 +1,7 @@
 package com.aang23.undergroundbiomes.blocks;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import com.aang23.undergroundbiomes.UndergroundBiomes;
@@ -7,16 +9,25 @@ import com.aang23.undergroundbiomes.api.enums.UBBlock;
 import com.aang23.undergroundbiomes.api.enums.UBStoneStyle;
 import com.aang23.undergroundbiomes.api.enums.UBStoneType;
 
+import net.minecraft.block.AnvilBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldReader;
@@ -34,6 +45,7 @@ public class UBOre extends Block implements UBBlock {
     public UBOre(Block baseOre, BlockState baseState, UBStoneType stone_type, String sub_stone_name,
             boolean useAlphaBlending) {
         super(Properties.create(baseOre.getMaterial(baseState)));
+        System.out.println(baseOre);
         this.baseOre = baseOre;
         this.baseState = baseState;
         this.stone_type = stone_type;
@@ -58,6 +70,78 @@ public class UBOre extends Block implements UBBlock {
     @Override
     public UBStoneType getStoneType() {
         return stone_type;
+    }
+
+    @Override
+    protected void fillStateContainer(net.minecraft.state.StateContainer.Builder<Block, BlockState> builder) {
+        // This method gets called in our parent's constructor... So this would crash
+        // since baseOre would be null
+        if (baseOre == null) {
+            super.fillStateContainer(builder);
+            return;
+        }
+
+        // Reflection to access private method on baseOre
+        try {
+            Method fillStateMethod = null;
+            fillStateMethod = baseOre.getClass().getDeclaredMethod("fillStateContainer",
+                    new Class[] { net.minecraft.state.StateContainer.Builder.class });
+            fillStateMethod.setAccessible(true);
+            fillStateMethod.invoke(Blocks.REDSTONE_ORE, builder);
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockState state, Direction facing, BlockState state2, IWorld world,
+            BlockPos pos1, BlockPos pos2, Hand hand) {
+        // TODO Auto-generated method stub
+        return baseOre.getStateForPlacement(state, facing, state2, world, pos1, pos2, hand);
+    }
+
+    @Override
+    public BlockState getStateAtViewpoint(BlockState state, IBlockReader world, BlockPos pos, Vec3d viewpoint) {
+        // TODO Auto-generated method stub
+        return baseOre.getStateAtViewpoint(state, world, pos, viewpoint);
+    }
+
+    @Override
+    public INamedContainerProvider getContainer(BlockState state, World worldIn, BlockPos pos) {
+        // TODO Auto-generated method stub
+        return baseOre.getContainer(state, worldIn, pos);
+    }
+
+    @Override
+    public BlockState getExtendedState(BlockState state, IBlockReader world, BlockPos pos) {
+        // TODO Auto-generated method stub
+        return baseOre.getExtendedState(state, world, pos);
+    }
+
+    @Override
+    public StateContainer<Block, BlockState> getStateContainer() {
+        // TODO Auto-generated method stub
+        return baseOre.getStateContainer();
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        // TODO Auto-generated method stub
+        return baseOre.getStateForPlacement(context);
+    }
+
+    @Override
+    public void onBlockClicked(BlockState state, World worldIn, BlockPos pos, PlayerEntity player) {
+        // TODO Auto-generated method stub
+        baseOre.onBlockClicked(state, worldIn, pos, player);
+    }
+
+    @Override
+    public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn,
+            BlockRayTraceResult hit) {
+        // TODO Auto-generated method stub
+        return baseOre.onBlockActivated(state, worldIn, pos, player, handIn, hit);
     }
 
     @Override
@@ -113,7 +197,7 @@ public class UBOre extends Block implements UBBlock {
 
     @Override
     public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
-        // baseOre.onEntityWalk(worldIn, pos, entityIn); //TODO fix
+        baseOre.onEntityWalk(worldIn, pos, entityIn); // TODO fix
     }
 
     @Override
